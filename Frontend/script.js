467e7d3a48944c0adcc965470a593b427ed8dd7d
Harshika <sharshika408@gmail.com>
2025-09-04 23:12:05 +0530

    Fixed API URL to /chat

--- Frontend/script.js
+++ Frontend/script.js
@@ -111,7 +111,7 @@ document.addEventListener('DOMContentLoaded', () => {
     async function getAIResponse(userText) {
         const bodyData = { text: userText, lang: currentLang, mode: currentMode, scenario: currentScenario };
         try {
-            const response = await fetch('https://speakgenie-internship-task-3.onrender.com', {
+            const response = await fetch('https://speakgenie-internship-task-3.onrender.com/chat', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(bodyData),
